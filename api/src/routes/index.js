568aeca1f3948f6b891fbb4dd322514379b3035d
Nestor <nestorariascataldi@gmail.com>
2022-07-08 18:15:09 -0300

    routes Character

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const axios = require('axios');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,48 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-
+const getApiInfo = async () => {
+  const apiUrl = await axios.get('https://breakingbadapi.com/api/characters');
+  const apiInfo = await apiUrl.data.map(el => {
+    return {
+      id: el.char_id,
+      name: el.name,      //aquí se puede unificar datos, ya que al "name:" le podemos asignar como querramos
+      birthday: el.birthday,
+      occupation: el.occupation.map(el => el),
+      img: el.img,
+      status: el.status,
+      nickname: el.nickname,
+      appearance: el.appearance.map(el => el)
+    }
+  });
+  return apiInfo;
+}
+const getDbInfo = async () => {
+  return await Character.findAll({
+    include: {
+      model: Occupation,
+      attributes: ['name'],
+      through: {
+        attributes: []
+      }
+    }
+  })
+};
+//aquí no sé si es necesario el async, ya que ya esta cargado.
+const getAllCharacter = async () => {
+  const apiInfo = await getApiInfo();
+  const dbInfo = await getDbInfo();
+  const infoTotal = apiInfo.concat(dbInfo);
+  return infoTotal;
+}
+router.get('/characters', async (req, res) => {
+  const name = req.query.name ;
+  let charactersTotal = await getAllCharacter();
+    if(name){
+      let characterName = await charactersTotal.filter(el => el.name.toLowerCase().include(name.toLowerCase())); //el include(name). name es lo que le paso por el query
+      characterName.length ? res.status(200).send(characterName) : res.status(404).send('No está el personaje');
+    } else {
+      res.status(200).send(charactersTotal)
+    }
+});
 module.exports = router;
