4017a20a86f9fe2d26b52fd0741142f0dbe5a367
Nestor <nestorariascataldi@gmail.com>
2022-07-08 22:24:32 -0300

    occupations db crash part1

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 const axios = require('axios');
-const {Character, Occupation} = require('../db')
+const { Character, Occupation } = require('../db')
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -44,14 +44,33 @@ const getAllCharacter = async () => {
   return infoTotal;
 }
 router.get('/characters', async (req, res) => {
-  const name = req.query.name ;
+  const name = req.query.name;
   let charactersTotal = await getAllCharacter();
-  // el includes(name). name es lo que le paso por el query
-    if(name){
-      let characterName = await charactersTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase())); 
-      characterName.length ? res.status(200).send(characterName) : res.status(404).send('No está el personaje');
-    } else {
-      res.status(200).send(charactersTotal)
-    }
+  // el includes(name) name es lo que le paso por el query
+  //el.name.toLowerCase() es la parte de la api walter y .includes(name.toLowerCase()) es para comparar como una busqueda más global.
+  if (name) {
+    let characterName = await charactersTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase()));
+    characterName.length ? res.status(200).send(characterName) : res.status(404).send('No está el personaje');
+  } else {
+    res.status(200).send(charactersTotal)
+  }
 });
+
+router.get('/occupations', async (req, res) => {
+  const occupationsApi = await axios.get('https://breakingbadapi.com/api/characters');
+  const occupations = occupationsApi.data.map(el => el.occupation);
+  const occEach = occupations.map(el => {
+    for (let i = 0; i < el.length; i++) return el[i]});
+console.error(occEach);
+  occEach.forEach(el => {
+    Occupation.findOrCreate({
+      where: { name: el }
+    })
+  });
+const allOccupations = await Occupation.findAll();
+res.send(allOccupations);
+})
+
+
+
 module.exports = router;
